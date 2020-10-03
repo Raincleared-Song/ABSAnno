fd8ee95cb56ebd42512fb2c8b388a2d056cc3423
宋晨阳 <2018013460@secoder.net>
2020-10-03 15:18:42 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+python manage.py makemigrations absanno_app
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('scy18', '', 'scy20000827')" | python manage.py shell
 python manage.py runserver 0.0.0.0:80
