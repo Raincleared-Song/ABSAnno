e0867676da7d1ba35820ba0937bf0124a1b436f5
王隽伟 <2018013446@secoder.net>
2020-10-03 10:19:54 +0800

    第二次修改url，以及重新merge

--- absanno_app/urls.py
+++ absanno_app/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
 
     path('', views.hello_world, name='hello_world'),
-    path('absanno/login', views.logIn, name='log_in'),
-    path('absanno/logout', views.logOut, name='log_out'),
-    path('absanno/square', views.userShow, name='square_show'),
-    path('absanno/mission', views.missionShow, name='mission_show'),
-    path('absanno/upload', views.upload, name='upload_mission')
+    path('login', views.logIn, name='log_in'),
+    path('logout', views.logOut, name='log_out'),
+    path('square', views.userShow, name='square_show'),
+    path('mission', views.missionShow, name='mission_show'),
+    path('upload', views.upload, name='upload_mission')
 
 ]
