a04f3377b48be54280724058b97151f921cab078
谢昱清 <2018013428@secoder.net>
2020-11-07 19:31:29 +0800

    add new feature to picture #36

--- absanno_app/groundPic.py
+++ absanno_app/groundPic.py
@@ -1,8 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def generatePic(basePic, title, generatedPic, num, coin):
-	title_font = ImageFont.truetype("../pics/Deng.ttf", size=58)
+def generatePic(basePic, title, generatedPic, num, coin, ddl):
+	title_font = ImageFont.truetype("../pics/Deng.ttf", size=55)
 	info_font = ImageFont.truetype("../pics/simfang.ttf", size=30)
+	# info_font2 = ImageFont.truetype("../pics/Deng.ttf", size=20)
 	img = Image.open(basePic)
 	bg_size = img.size
 	# print(img)
@@ -20,13 +21,15 @@ def generatePic(basePic, title, generatedPic, num, coin):
 	img = Image.open(basePic)
 	image = Image.blend(img, image, 0.7)
 	draw = ImageDraw.Draw(image)
+	# info2 = "发布者：" + str(user)
 
 	colort="#ffffff"
 	color = "#333333"
-	info1 = "题目数量：" + str(num) + "    悬赏金额：" + str(coin)
+	info1 = "题数：" + str(num) + "  赏金：" + str(coin) + "  截止：" + str(ddl)
+
 
 	title_size = title_font.getsize(title)
-	title_coordinate = int((bg_size[0] - title_size[0]) / 2), int((bg_size[1] - title_size[1]) / 2) -35
+	title_coordinate = int((bg_size[0] - title_size[0]) / 2), int((bg_size[1] - title_size[1]) / 2) - 35
 	draw.text(title_coordinate, u'%s' % title, colort, title_font)
 
 	info_size = info_font.getsize(info1)
@@ -34,12 +37,12 @@ def generatePic(basePic, title, generatedPic, num, coin):
 	draw.text(info_coordinate, u'%s' % info1, color, info_font)
 
 	# info_size = info_font.getsize(info2)
-	# info_coordinate = int((bg_size[0] - info_size[0]) / 2), int((bg_size[1] - info_size[1]) / 2) + 80
-	# draw.text(info_coordinate, u'%s' % info2, color, info_font)
+	# info_coordinate = int((bg_size[0] - info_size[0]) / 2), int((bg_size[1] - info_size[1]) / 2) - 2
+	# draw.text(info_coordinate, u'%s' % info2, colort, info_font2)
 	image.save(generatedPic)
 
 # test only
 # for i in range(1, 8):
 # 	generatePic("../pics/"+str(i)+".jpg",
 # 				"选择题1号测试", "../pics/"+str(i)+"-1.png",
-# 				5, 10)
+# 				5, 10, "2020-11-07")
